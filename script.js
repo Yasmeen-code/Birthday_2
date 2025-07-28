ee4598253be2291beb9765f2e8869fa19c8ebee6
Yasmeen_sale7 <yasmeensaleh747@gmail.com>
2025-07-28 18:48:09 +0300

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+var slideIndex = 0;
+var slides = document.querySelectorAll('.slide');
 
+function showSlides() {
+    slides.forEach((slide, index) => {
+        slide.classList.remove('active');
+        if(index === slideIndex) {
+            slide.classList.add('active');
+        }
+    });
+    slideIndex++;
+    if(slideIndex >= slides.length) {
+        slideIndex = 0;
+    }
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
+
+window.onload = function() {
+    showSlides();
+};
